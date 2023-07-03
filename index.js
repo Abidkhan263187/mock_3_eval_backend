3db1b57a6832e2a41bd36aaa48e7f375df4b00da
abidkhan263187 <abidkhan263187@gmail.com>
2023-07-03 22:50:28 +0530

    3 hour backend

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express')
 require('dotenv').config()
+const cors=require("cors");
+
+
 const { connection } = require('./config/db');
 const { dashBoardRouter } = require('./RouterController/dashboardRouter');
 const { userRouter } = require('./RouterController/userRouter');
@@ -7,6 +10,11 @@ const { userRouter } = require('./RouterController/userRouter');
 
 const app = express();
 app.use(express.json())
+
+app.use(cors({
+    origin: '*'
+  })) 
+
 
 app.use('/', userRouter)
 
